ba4238d06a2c1a74ae2d9fcfbf70508b18dd5ef2
Harclay <harrisclayton111@gmail.com>
2023-05-19 23:45:26 -0500

    part 1 done

---
+++ api/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const usersRouter = express.Router();
+const { getAllUsers } = require('../db/index');
+
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+  next();
+});
+
+usersRouter.get('/', async (req, res) => {
+  const users = await getAllUsers();
+  res.send({ users });
+});
+
+module.exports = usersRouter;
